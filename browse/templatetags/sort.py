90798a03f9d6c901f9b867dd8798f25344e9dd17
Paul Richter <nobuyuki.richter@gmail.com>
2024-09-13 23:52:10 +0900

    sort_by_season places unsortables first

--- browse/templatetags/sort.py
+++ browse/templatetags/sort.py
@@ -8,5 +8,15 @@ register = template.Library()
 @register.filter(name='sort_by_season')
 def sort_by_season(queryset):
     """Sort by season; Can be used for cars or engines or anything that has an earliest_season() method."""
-    return sorted(queryset, key=lambda c: c.earliest_season())
+    #Unlike earliest_season, where we can discard None items, here we need to deal with them first
+    #filter out any item with no earliest season, since they can not be sorted
+    unsortables = []
+    sortables = []
+    for i in queryset:
+        if i.earliest_season() is None :
+            unsortables.append(i)
+        else:
+            sortables.append(i)
+
+    return unsortables + sorted(sortables, key=lambda c: c.earliest_season())
 
