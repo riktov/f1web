e491116c2102eb16914e87c2939cc4e6074bb8aa
Paul Richter <nobuyuki.richter@gmail.com>
2024-10-03 22:52:34 +0900

    Admin list_display

--- f1web/admin.py
+++ f1web/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Car, Constructor, Driver, TeamManager, Engine, EngineMaker, Season, DrivingContract, CarNumber, Rule, ConstructorTransfer
+from .models import Car, Constructor, Driver, Engine, EngineMaker, Season, DrivingContract, CarNumber, Rule, ConstructorTransfer
 
 # Autopopulate slugs
 # https://www.w3schools.com/django/django_slug_field.php
@@ -21,15 +21,24 @@ class CarAdmin(admin.ModelAdmin):
     # list_display = ("constructor", "name",)
     prepopulated_fields = {"slug": ("constructor", "name",)}
 
+class CarNumberAdmin(admin.ModelAdmin):
+    list_display = ("season", "team",)
+
+class DrivingContractAdmin(admin.ModelAdmin):
+    list_display = ("season", "driver", "team",)
+
+# class ConstructorTransferAdmin(admin.ModelAdmin):
+#     list_display = ("season", "previous", "new")
+
 admin.site.register(Driver, DriverAdmin)
 admin.site.register(Constructor, ConstructorAdmin)
 
 admin.site.register(Car, CarAdmin)
-admin.site.register(TeamManager)
+# admin.site.register(TeamManager)
 admin.site.register(Season)
 # admin.site.register(Drive)
-admin.site.register(DrivingContract)
-admin.site.register(CarNumber)
+admin.site.register(DrivingContract, DrivingContractAdmin)
+admin.site.register(CarNumber, CarNumberAdmin)
 admin.site.register(Engine)
 admin.site.register(EngineMaker)
 admin.site.register(Rule)
