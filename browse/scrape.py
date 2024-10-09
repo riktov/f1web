d12d42068cf42b4c44feaa05b5f477c8dbed70de
Paul Richter <nobuyuki.richter@gmail.com>
2024-10-09 11:17:56 +0900

    fully working

--- browse/scrape.py
+++ browse/scrape.py
@@ -1,28 +1,39 @@
 """Module for scraping information from Wikipedia"""
 
+import re
 import urllib.request
 from bs4 import BeautifulSoup
 from bs4 import element
+from pprint import pp
 
 WIKIPEDIA_BASE_URL = "https://en.wikipedia.org/wiki/"
 
-def parse_engine_count(s):
-    s = s.strip()
-    parts = s.split('×')
-    return int(parts[0].strip())
+COL_CAR = 5 
+COL_TEAM = 0
+XCOL_CAR=999
 
-def parse_engine_name(s):
-    parts = s.split(' ')
-    
-    #if the first part has a hyphen, then that is a British maker name
-    if '-' in parts[0]:
-        maker = parts[0]
-        designation = ' '.join(parts[1:])
-        return (maker, designation)
-    else:   
-        designation = parts[-1]
-        maker = ' '.join(parts[0:-1]) 
-        return (maker, designation)
+
+def parse_team_or_driver(tr):
+    return [ s for s in tr.strings if s != ' ' and s != '\n']
+    return tr.string
+
+def is_entrant_or_driver(td):
+    return type(td) is element.Tag and td.find_all("span", attrs={"class": "flagicon"})
+
+def parse_head_row(tr):
+    return parse_team_or_driver(tr[1])
+
+def parse_driver(td):
+    pass
+
+def blank_entry():
+    return {
+        "drivers": [],
+        "cars": []
+    }
+
+def element_joined_strings(elem):
+    return ' '.join([s for s in elem.stripped_strings])
 
 def scrape_season_cars(article_name):
     url = WIKIPEDIA_BASE_URL + article_name
@@ -33,34 +44,91 @@ def scrape_season_cars(article_name):
     sect_hed_dc = soup.find('h2', id='Drivers_and_constructors')
     sect_hed_td = soup.find('h2', id='Teams_and_drivers')
 
-    sect_hed = sect_hed_dc
-    if not sect_hed:
-        sect_hed = sect_hed_td
+    sect_hed = sect_hed_dc or sect_hed_td
+    # if not sect_hed:
+    #     sect_hed = sect_hed_td
 
     mw_heading = sect_hed.parent
     table = mw_heading.findNextSibling(attrs={"class":"wikitable"})
     tbody = table.find('tbody')
 
+    headings = table.find("thead")
+    print(headings)
+    entries = []
+
+    this_entry = blank_entry()
+    prev_number = 0
+
     for tr in tbody.children:
         if type(tr) is element.Tag:
-            tds = tr.children
-            #if there are more than three tds, it is the first row
-            # if len(tds) > 3:
-            # print("!!------!!")
-            idx = 0 
-            for td in tds:
-                # print(f"%%---{idx}---%%")
-                if idx == 5:
-                    a_tags = td.find_all('a')
-                    for tag in a_tags:
-                        print(tag['title'])
-                idx += 1
+            if tr.parent is headings:
+                print("This is the header, which we should skip.")
 
-    return {
-        "engine_count": 1,
-        "engine_name": 2
-    }
+            span_flags = tr.find_all("span", attrs={"class": "flagicon"})
+
+            tds = [ td for td in tr.children if td != '\n' ] 
+            # print(tds)
+
+            if len(span_flags) == 0:
+                print("This is a row without a flag.")
+                continue  
+
+            if len(span_flags) > 1:
+                # save the current, create new one
+                if "constructor" in this_entry:
+                    entries.append(this_entry)
+                    this_entry = blank_entry()
+
+                # tds = span_flags[0].parent.parent
+
+                td_entrant = tds[0]
+                entrant = td_entrant.get_text(' ', strip=True)
+
+                print("\nEntrant:", entrant)
+                this_entry["entrant"] = entrant
+                
+                td_constructor = tds[1]
+                #We want only the chassis constructor; "McLaren", not "McLaren-Honda"
+                constructor = td_constructor.contents[0].string
+                print("Constructor:", constructor)
+                this_entry["constructor"] = constructor
+
+                td_cars = tds[2]
+                cars = [ elem.string for elem in td_cars]
+                print(cars)
+                cars = [ str(c).strip() for c in cars if c != '\n' and c is not None]
+                
+                this_entry["cars"] = cars
+                print("Cars:", cars)
+
+            td_rounds = tds[-1]
+            rounds = td_rounds.get_text(' ', strip=True)
+            
+            td_driver = tds[-2]
+            driver = td_driver.get_text(' ', strip=True)
+
+            number = None
+
+            if len(tds) > 2:
+                td_number = tds[-3]
+                num_text = td_number.get_text(' ', strip=True)
+                num_text = re.sub(r"\D", "", num_text) 
+                number = int(num_text)
+            else:
+                number = prev_number
+            
+            print("Driver:", driver)
+
+            driver_info = {
+                "driver": driver,
+                "rounds": rounds,
+                "number": number
+            }
+            this_entry["drivers"].append(driver_info)
+
+            prev_number = number
+    return entries
 
 if __name__ == "__main__":
-    specs = scrape_season_cars("1989_Formula_One_World_Championship")
-    print(specs)
+    specs = scrape_season_cars("1994_Formula_One_World_Championship")
+    pp(specs)
