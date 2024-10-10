94a3d9d516a4fbb6ee2ad21260c1279c4bf59c98
Paul Richter <nobuyuki.richter@gmail.com>
2024-10-10 17:22:02 +0900

    test_scrape

---
+++ browse/test_scrape.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from browse.scrape import scrape_season_cars
+
+
+class ScrapeTest(TestCase):
+    def setUp(self):
+        self.seasons = [
+            "1987_Formula_One_World_Championship",
+            "1988_Formula_One_World_Championship",
+            "1989_Formula_One_World_Championship",
+        ]
+
+    def test_valid_constructors(self):
+        for season in self.seasons:
+            specs = scrape_season_cars(season)
+            for constructor in specs:
+                self.assertIsNotNone(constructor['constructor'])
+                self.assertIsNotNone(constructor['cars'])
