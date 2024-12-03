c45de079f0e074d6f232577a9248d5d66894dccf
Paul Richter <nobuyuki.richter@gmail.com>
2024-12-03 17:18:08 +0900

    basic functionality

--- game/views.py
+++ game/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from browse import forms
-from f1web.models import Driver
+from f1web.models import Driver, DrivingContract, Season
 from game.forms import DriverSelectionForm
 
 def index(request):
@@ -13,17 +13,39 @@ def index(request):
         }
         return render(request, "game/index.html", context)
     
+
     driver_from = Driver.objects.get(pk = get_dict["driver_from"])
     driver_to = Driver.objects.get(pk = get_dict["driver_to"])
-    
-    seasons = driver_from.seasons
-    seasons = sorted(seasons, key=lambda s:s.year)
+    driver = driver_from 
+
+    if get_dict.get("driver"):
+        driver = Driver.objects.get(pk = get_dict["driver"])
+
+    season = get_dict.get("season")
+
+    if season:
+        season = Season.objects.get(pk = int(season))
+        
+        dcs = DrivingContract.objects.filter(driver=driver, season=season)
+        team = dcs[0].team
+        drives = team.drives.filter(season = season)
+        drives = drives.exclude(driver = driver)
+        teammates = [dr.driver for dr in drives]
+        context = {
+            "driver_from": driver_from,
+            "driver_to": driver_to,
+            "driver": driver,
+            "teammates": teammates
+        }
+        return render(request, "game/select_teammate.html", context)
+
+    drives = sorted(driver.drives(), key=lambda d:d.season)
     
     context = {
         "driver_from": driver_from,
-        "driver_to": driver_from,
         "driver_to": driver_to,
-        "seasons": seasons
+        "driver": driver,
+        "drives": drives
     }
 
-    return render(request, "game/play.html", context)
+    return render(request, "game/select_season.html", context)
