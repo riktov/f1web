8c18257b8ec9bd341e11eadbdf9aded06ccfb36d
Paul Richter <nobuyuki.richter@gmail.com>
2024-12-05 11:26:51 +0900

    add teaming test

--- game/tests.py
+++ game/tests.py
@@ -1,27 +1,36 @@
 from django.test import TestCase
 
 from f1web.models import Constructor, Driver, DrivingContract, Season
-from game.queries import teammates_all, teammates_in_season
+from game.queries import teamings, teammates_all, teammates_in_season
 
 # Create your tests here.
 
 class TeammatesTest(TestCase):
     """Test Case for drivers' teammates"""
     def setUp(self):
-        Season.objects.create(year=1980)
-        Season.objects.create(year=1981)
-        Season.objects.create(year=1982)
-        Season.objects.create(year=1986)
+        s1980 =Season.objects.create(year=1980)
+        s1981 = Season.objects.create(year=1981)
+        s1982 = Season.objects.create(year=1982)
+        s1986 = Season.objects.create(year=1986)
 
-        Constructor.objects.create(name="Ferrari")
-        Constructor.objects.create(name="Brabham")
-        Constructor.objects.create(name="McLaren")
-        Constructor.objects.create(name="Williams")
+        ferrari = Constructor.objects.create(name="Ferrari")
+        brabham = Constructor.objects.create(name="Brabham")
+        mclaren = Constructor.objects.create(name="McLaren")
+        williams = Constructor.objects.create(name="Williams")
 
-        Driver.objects.create(name="Nelson")
-        Driver.objects.create(name="Niki")
-        Driver.objects.create(name="Riccardo")
-        Driver.objects.create(name="Nigel")     
+        nelson = Driver.objects.create(name="Nelson")
+        niki = Driver.objects.create(name="Niki")
+        riccardo =Driver.objects.create(name="Riccardo")
+        nigel = Driver.objects.create(name="Nigel")     
+        alain = Driver.objects.create(name = "Alain")
+
+        DrivingContract.objects.create(driver=nelson, team=brabham, season=s1982)
+        DrivingContract.objects.create(driver=riccardo, team=brabham, season=s1982)
+        DrivingContract.objects.create(driver=niki, team=ferrari, season=s1982)
+        DrivingContract.objects.create(driver=nelson, team=williams, season=s1986)
+        DrivingContract.objects.create(driver=nigel, team=williams, season=s1986)
+        DrivingContract.objects.create(driver=alain, team=mclaren, season=s1986)
+        
 
     def test_teammates_in_season(self):
         """A driver's teammates in a single season can be acquired"""
@@ -29,44 +38,40 @@ class TeammatesTest(TestCase):
         riccardo = Driver.objects.get(name="Riccardo")
         niki = Driver.objects.get(name="Niki")
 
-        brabham = Constructor.objects.get(name="Brabham")
-        ferrari = Constructor.objects.get(name="Ferrari")
         s1982 = Season.objects.get(year = 1982)
-
-        DrivingContract.objects.create(driver=nelson, team=brabham, season=s1982)
-        DrivingContract.objects.create(driver=riccardo, team=brabham, season=s1982)
-        DrivingContract.objects.create(driver=niki, team=ferrari, season=s1982)
                 
-        teammates = teammates_in_season(nelson, s1982)
+        teammates82 = teammates_in_season(nelson, s1982)
 
-        self.assertIn(riccardo, teammates)
-        self.assertNotIn(niki, teammates)
+        self.assertIn(riccardo, teammates82)
+        self.assertNotIn(niki, teammates82)
     
     def test_teammates_all(self):
         """a driver's teammates in all seasons can be acquired"""
         nelson = Driver.objects.get(name="Nelson")
         riccardo = Driver.objects.get(name="Riccardo")
         nigel = Driver.objects.get(name="Nigel")
+        alain = Driver.objects.get(name="Alain")
+
+        nelsons_teammates = teammates_all(nelson)
+
+        self.assertIn(riccardo, nelsons_teammates)
+        self.assertIn(nigel, nelsons_teammates)
+        self.assertNotIn(alain, nelsons_teammates)
+
+    def test_teamed(self):
+        nelson = Driver.objects.get(name="Nelson")
+        riccardo = Driver.objects.get(name="Riccardo")
+        nigel = Driver.objects.get(name="Nigel")
         niki = Driver.objects.get(name="Niki")
-        
-        brabham = Constructor.objects.get(name="Brabham")
-        williams = Constructor.objects.get(name="Williams")
-        ferrari = Constructor.objects.get(name="Ferrari")
-        s1982 = Season.objects.get(year = 1982)
-        s1986 = Season.objects.get(year = 1986)
 
-        DrivingContract.objects.create(driver=nelson, team=brabham, season=s1982)
-        DrivingContract.objects.create(driver=nelson, team=williams, season=s1986)
-        DrivingContract.objects.create(driver=riccardo, team=brabham, season=s1982)
-        DrivingContract.objects.create(driver=niki, team=ferrari, season=s1982)
-        DrivingContract.objects.create(driver=nigel, team=williams, season=s1986)
-
-        teammates = teammates_all(nelson)
-
-        self.assertIn(riccardo, teammates)
-        self.assertIn(nigel, teammates)
-        self.assertNotIn(niki, teammates)
+        s1986 = Season.objects.get(year=1986)
+        williams = Constructor.objects.get(name = "Williams") 
 
 
+        gig1 = teamings(nelson, nigel)[0]
+        season, teams = gig1
+
+        self.assertEqual(season, s1986)
+        self.assertIn(williams, teams)
 
 
